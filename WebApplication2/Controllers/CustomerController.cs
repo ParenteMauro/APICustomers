ef441a0222cb0141420653bb86454d7fc8444780
Mauro Parente <mauroparenteinformatica@gmail.com>
2025-02-03 21:08:05 -0300

    Creacion de clase Customer y CustomerController

---
+++ WebApplication2/Controllers/CustomerController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Dtos;
+
+namespace WebApplication2.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : Controller
+    {
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(CustomerDto))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetCustomer(long id)
+        {
+            var vacio = new CustomerDto();
+
+            return new OkObjectResult(vacio);
+            
+        }
+
+        [HttpGet]
+        
+        public async Task<List<CustomerDto>> GetCustomers()
+        {
+            throw new NotImplementedException();
+
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<bool> DeleteCustomer(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(CustomerDto))]
+        
+
+        public async Task<IActionResult> CreateCustomer(CreateCustomerDto customer)
+        {
+            var vacio = new CustomerDto();
+
+            return new CreatedResult($"https://localhost:7180/api/customer/{vacio.Id}", null);
+
+            //throw new NotImplementedException();
+        }
+
+        [HttpPut]
+
+        public async Task<CustomerDto> UpdateCustomer(CustomerDto customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApplication2/Dtos/CreateCustomerDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Dtos
+{
+    public class CreateCustomerDto
+    {
+
+        [Required (ErrorMessage = "El nombre es requerido")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "El apellido es requerido")]
+        public string LastName { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "El Email es invalido")]
+        public string Email { get; set; }
+
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
